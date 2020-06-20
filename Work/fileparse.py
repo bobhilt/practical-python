b0ee77dabdb0d5a40f82dc1a11bb8e14859ccfc8
Bob Hiltner <bobhilt@gmail.com>
2020-06-20 12:40:04 -1000

    included updates in main file

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,24 +1,29 @@
 # fileparse.py
 #
-# Exercise 3.3
+# (through) Exercise 3.9
 #
 ''' parse a csv file into a list of records
-    (assumes header row)
+    (passes has_header flag), optional delimiter.
+    Add exception for invalid select, warning for data errors.
 '''
 
 import csv
-def parse_csv(filename, select=None):
+def parse_csv(filename, select=None, recast=None, has_headers=False, delimiter=',', silence_errors=False):
+    if select and not has_headers:
+        raise(RuntimeError,"Select requires headers.")
+        
     with open(filename) as f:
-        rows = csv.reader(f)
+        rows = csv.reader(f, delimiter=delimiter)
+    
+        if has_headers:
+            headers = next(rows)
 
-        headers = next(rows)
-
-        if select:              # return only passed columns
-            indices = [headers.index(column_name) for column_name in select]
-            headers  = select
+            if select:              # return only passed columns
+                indices = [headers.index(column_name) for column_name in select]
+                headers  = select
 
         records = []
-        for row in rows:
+        for rownum, row in enumerate(rows):
             if not row:         #skip empty rows
                 continue
             
@@ -26,7 +31,20 @@ def parse_csv(filename, select=None):
             if select:
                 row = [row[index] for index in indices]
 
-            record = dict(zip(headers,row))
-            records.append(record)
+            try:
+                if recast:
+                    row = [f(value) for f,value in zip(recast, row)]
+
+                if has_headers:
+                    record = dict(zip(headers,row))
+                else:
+                    record = tuple(row)
+                records.append(record)
     
+            except ValueError as e:
+                if not silence_errors:
+                    print(f'Row {rownum}: {e}: {row}')
+                continue
+
     return records
+            
